1481f88524c5cbb608377692986d86b8ac7d2fea
heytitle <pat.chormai@gmail.com>
2019-11-21 16:17:38 +0100

    fix var name

--- scripts/train_2d_vary_opt.sh
+++ scripts/train_2d_vary_opt.sh
@@ -2,7 +2,7 @@
 
 DATASET=$1
 
-python3 train.py --epoch 100 --dataset DATASET --strategy="oneshot"  "vdb/e1:1024|e2:1024|z:2"
-python3 train.py --epoch 100 --dataset DATASET --strategy="seq/d:1|e:5"  "vdb/e1:1024|e2:1024|z:2"
-python3 train.py --epoch 100 --dataset DATASET --strategy="seq/d:1|e:10"  "vdb/e1:1024|e2:1024|z:2"
-python3 train.py --epoch 100 --dataset DATASET --strategy="seq/d:1|e:20"  "vdb/e1:1024|e2:1024|z:2"
+python3 train.py --epoch 100 --dataset $DATASET --strategy="oneshot"  "vdb/e1:1024|e2:1024|z:2"
+python3 train.py --epoch 100 --dataset $DATASET --strategy="seq/d:1|e:5"  "vdb/e1:1024|e2:1024|z:2"
+python3 train.py --epoch 100 --dataset $DATASET --strategy="seq/d:1|e:10"  "vdb/e1:1024|e2:1024|z:2"
+python3 train.py --epoch 100 --dataset $DATASET --strategy="seq/d:1|e:20"  "vdb/e1:1024|e2:1024|z:2"
