76bdc841a4e02a0b6f2e7a9dd4318c2f47d9e605
heytitle <pat.chormai@gmail.com>
2019-11-22 18:13:56 +0100

    update script

--- scripts/train_2d_vary_opt.sh
+++ scripts/train_2d_vary_opt.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 DATASET=$1
+BETA=$2
+M=$3
+K=$4
 
-python3 train.py --epoch 100 --dataset $DATASET --strategy="oneshot"  "vdb/e1:1024|e2:1024|z:2"
-python3 train.py --epoch 100 --dataset $DATASET --strategy="seq/d:1|e:5"  "vdb/e1:1024|e2:1024|z:2"
-python3 train.py --epoch 100 --dataset $DATASET --strategy="seq/d:1|e:10"  "vdb/e1:1024|e2:1024|z:2"
-python3 train.py --epoch 100 --dataset $DATASET --strategy="seq/d:1|e:20"  "vdb/e1:1024|e2:1024|z:2"
+time python3 train.py --epoch 100 --dataset $DATASET --beta $BETA -M $M --strategy="oneshot"  "vdb/e1:1024|e2:1024|z:$K"
+time python3 train.py --epoch 100 --dataset $DATASET --beta $BETA -M $M --strategy="seq/d:1|e:5"  "vdb/e1:1024|e2:1024|z:$K"
+time python3 train.py --epoch 100 --dataset $DATASET --beta $BETA -M $M --strategy="seq/d:1|e:10"  "vdb/e1:1024|e2:1024|z:$K"
+time python3 train.py --epoch 100 --dataset $DATASET --beta $BETA -M $M --strategy="seq/d:1|e:20"  "vdb/e1:1024|e2:1024|z:$K"
+time python3 train.py --epoch 100 --dataset $DATASET --beta $BETA -M $M --strategy="seq/d:5|e:1"  "vdb/e1:1024|e2:1024|z:$K"
+time python3 train.py --epoch 100 --dataset $DATASET --beta $BETA -M $M --strategy="seq/d:10|e:1"  "vdb/e1:1024|e2:1024|z:$K"
+time python3 train.py --epoch 100 --dataset $DATASET --beta $BETA -M $M --strategy="seq/d:20|e:1"  "vdb/e1:1024|e2:1024|z:$K"
