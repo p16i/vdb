9dec49a8ed0fd5590d2b4677a7b53a50c3a6f265
heytitle <pat.chormai@gmail.com>
2019-11-24 18:08:42 +0100

    update script

--- scripts/train_for_bottleneck.sh
+++ scripts/train_for_bottleneck.sh
@@ -2,12 +2,15 @@ STRATEGIES=$2
 DATASET=$1
 K=256
 M=1
+OUTPUT="$DATASET--bottleneck-curve"
 
 BETA="0.1 0.01 0.001 0.0001 0.00001 0.000001 0.0000001 0.00000001 0.000000001"
 
 for s in $STRATEGIES; do
     for b in $BETA; do
         echo "$DATASET $s $b"
-        time python3 train.py --epoch 100 --dataset $DATASET --beta $b -M $M --strategy=$s  "vdb/e1:1024|e2:1024|z:$K"
+        time python3 train.py --epoch 100 --dataset $DATASET  \
+            --beta $b -M $M --strategy=$s  "vdb/e1:1024|e2:1024|z:$K" \
+            --output-dir $OUTPUT
     done
 done
