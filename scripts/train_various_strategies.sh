d988168b804118546d0d281587033e550f0d9746
heytitle <pat.chormai@gmail.com>
2019-12-08 19:39:25 +0100

    update train_various_strategies.sh

--- scripts/train_various_strategies.sh
+++ scripts/train_various_strategies.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
 DATASET=$1
-BETA=$2
-M=$3
-K=$4
 
-OUTPUT="$DATASET--vary-strategies"
+ALGOSLUG=`echo $SCRIPT | sed s/[.]/-/g | sed s/py$//g`
+OUTPUT="./artifacts/$ALGOSLUG/cherry-pick-after-merging-$DATASET--lr-decay-various-opts-M$M"
 
-STRATEGIES="oneshot seq/d:1|e:5 seq/d:1|e:10 seq/d:1|e:20 seq/d:5|e:1 seq/d:10|e:1 seq/d:20|e:1"
+#STRATEGIES="oneshot seq/d:5|e:1 seq/d:10|e:1 seq/d:20|e:1"
+#STRATEGIES="seq/d:5|e:1 seq/d:10|e:1 seq/d:20|e:1"
+#STRATEGIES="seq/d:10|e:1"
+STRATEGIES="oneshot"
+#STRATEGIES="alt/e:5|d:1 alt/e:10|d:1 alt/e:20|d:1"
 
-for s in $STRATEGIES; do
-    time python3 train.py \
-        --dataset $DATASET --beta $BETA -M $M \
-        --output-dir="./artifacts/$OUTPUT" \
-        --strategy="$s" "vdb/e1:1024|e2:1024|z:$K"
+for i in `seq $TOTAL_RUNS`; do
+        echo "------ RUN $i ------"
+        for s in $STRATEGIES; do
+                echo "Script: $SCRIPT |  STATS:$s DATASET=$DATASET BETA=$BETA M=$M K=$K -> $OUTPUT"
+            sbatch ./slurm_train.sh \
+                $SCRIPT \
+                --epoch $EPOCH \
+                --dataset $DATASET --beta $BETA -M $M \
+                --output-dir="$OUTPUT" \
+                --strategy="$s" "vdb/e1:1024|e2:1024|z:$K"
+           sleep 3 # preventing naming cache
+        done
 done
