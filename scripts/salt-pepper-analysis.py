83acc04022079d3099db80deed19478816899cf4
heytitle <pat.chormai@gmail.com>
2019-12-22 16:37:10 +0100

    refactor salt-pepper-analysis

--- scripts/salt-pepper-analysis.py
+++ scripts/salt-pepper-analysis.py
@@ -1,15 +1,17 @@
 """
 Usage:
-salt-pepper-analysis.py  [-p=<p>] <model-dir>
+salt-pepper-analysis.py  [-p=<p> -L=<L>] <model-dir>
 
 Options:
   -h --help     Show this screen.
-  -p=<p>            Probability of flipling, [default: 0.1,0.2,0.4]
+  -p=<p>        Probability of flipling, [default: 0.1,0.2,0.4]
+  -L=<L>        Number of prediction samplings, [default: 1, 12]
 """
 
 from docopt import docopt
 
 import glob
+import os
 import yaml
 
 import numpy as np
@@ -19,15 +21,29 @@ import tensorflow as tf
 from nets import load_model
 from datasets import get_dataset
 
-from perturbation import salt_paper_noise
+from perturbation import salt_pepper_noise
 
 # Remark: this should divide test_images.shape[0] without remainder
 BATCH_SIZE = 1000
-L=12
+L = 1
 
-def salt_pepper_analysis(model_path, probs):
+if __name__ == "__main__":
+    arguments = docopt(__doc__)
 
-    model, summary = load_model(model_path)
+    probs = list(map(float, arguments["-p"].split(",")))
+    Ls = list(map(int, arguments["-L"].split(",")))
+
+    model_dir = arguments['<model-dir>']
+
+    print(f"Salt-Pepper Analysis: {model_dir}")
+    print("Probabilities: ", probs)
+    print("L for samples: ", Ls)
+
+    models = glob.glob(f"{model_dir}/summary.yml")
+
+    print(f"We have {len(models)} models.")
+
+    model, summary = load_model(os.path.dirname(models[0]))
 
     _, (test_images, test_labels), _ = get_dataset(summary["dataset"])
 
@@ -36,45 +52,47 @@ def salt_pepper_analysis(model_path, probs):
 
     extreme_values = np.min(test_images), np.max(test_images)
 
+    del model
 
-    metrics = dict(L=L, batch_size=BATCH_SIZE, settings=[])
+    perturbed_image_set = dict()
 
-    for p in [0, *probs]:
-        perturbed_images = salt_paper_noise(
+    # include p=0 as well for sanity checks
+    probs = [0, *probs]
+
+    for p in probs:
+        # cache this pertubed images
+        perturbed_image_set[p] = salt_pepper_noise(
             test_images,
             p=p,
             extreme_values=extreme_values
         )
 
-        dataset = tf.data.Dataset.from_tensor_slices((perturbed_images, test_labels)) \
-            .shuffle(test_images.shape[0]).batch(BATCH_SIZE)
+    for i, model in enumerate(models):
+        path = os.path.dirname(model)
 
-        mean_acc = tf.keras.metrics.Mean()
-        for i, (x, y) in enumerate(dataset):
-            acc = model.compute_acc(x, y, L)
-            mean_acc.update_state(acc)
+        model, summary = load_model(path)
+        print(f"{i+1}: {path}")
 
-        mean_acc = float(mean_acc.result().numpy())
+        metrics = dict(batch_size=BATCH_SIZE, settings=[])
 
-        metrics['settings'].append(dict(p=p, accuracy=mean_acc))
+        for p in probs:
+            perturbed_images = perturbed_image_set[p]
 
-    with open(f"{model_path}/salt-pepper-analysis.yml", 'w') as f:
-        yaml.dump(metrics, f, default_flow_style=False)
+            ds = tf.data.Dataset \
+                .from_tensor_slices((perturbed_images, test_labels)) \
+                .shuffle(test_images.shape[0]).batch(BATCH_SIZE)
 
+            mean_acc = tf.keras.metrics.MeanTensor()
+            for i, (x, y) in enumerate(ds):
+                accs = list(map(lambda l: model.compute_acc(x, y, l), Ls))
+                mean_acc.update_state(accs)
 
-if __name__ == "__main__":
-    arguments = docopt(__doc__)
+            mean_acc = mean_acc.result().numpy()
 
-    probs = list(map(float, arguments['-p'].split(",")))
-    model_dir = arguments['<model-dir>']
+            for i, L in enumerate(Ls):
+                metrics['settings'].append(
+                    dict(p=p, L=L, accuracy=float(mean_acc[i]))
+                )
 
-    print(f"Salt-Pepper Analysis: {model_dir}")
-    print("Probabilities: ", probs)
-
-    models = glob.glob(f"{model_dir}/summary.yml")
-
-    print(f"We have {len(models)} models.")
-
-    for  model in models:
-        path = "/".join(model.split("/")[:-1])
-        salt_pepper_analysis(path, probs)
+        with open(f"{path}/salt-pepper-analysis.yml", 'w') as f:
+            yaml.dump(metrics, f, default_flow_style=False)
