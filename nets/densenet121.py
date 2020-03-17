9db5b8855d7a53bb0ae1013ce2fdd7f21e85df01
heytitle <pat.chormai@gmail.com>
2020-03-17 23:11:29 +0100

    fix dimensions

--- nets/densenet121.py
+++ nets/densenet121.py
@@ -5,6 +5,10 @@ from nets.base import BaseNet
 from efficientnet.tfkeras import EfficientNetB7
 from tensorflow.keras.applications.densenet import DenseNet121
 
+output_size = {
+    (32, 32, 3): (1024,)
+}
+
 class Net(BaseNet):
     def __init__(self, architecture, in_shape, num_class, cov_type,  batch_norm=True, beta=1e-3, M=1):
         super(Net, self).__init__(architecture, cov_type, num_class, beta, M)
@@ -12,7 +16,7 @@ class Net(BaseNet):
         self.encoder = tf.keras.Sequential(
             [
                 DenseNet121(input_shape=in_shape, weights=None, include_top=False),
-                tf.keras.layers.Lambda(tf.squeeze),
+                tf.keras.layers.Reshape(output_size[in_shape]),
                 tf.keras.layers.Dense(self.parameters_for_latent),
             ]
         )
