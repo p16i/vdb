49518c9a2ca3875f14d2b37aff28b49fc510f15f
heytitle <pat.chormai@gmail.com>
2020-03-17 22:15:45 +0100

    add densenet

---
+++ nets/densenet121.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+from nets.base import BaseNet
+
+from efficientnet.tfkeras import EfficientNetB7
+from tensorflow.keras.applications.densenet import DenseNet121
+
+class Net(BaseNet):
+    def __init__(self, architecture, in_shape, num_class, cov_type,  batch_norm=True, beta=1e-3, M=1):
+        super(Net, self).__init__(architecture, cov_type, num_class, beta, M)
+
+        self.encoder = DenseNet121(
+            weights=None,
+            classes=self.parameters_for_latent
+        )
