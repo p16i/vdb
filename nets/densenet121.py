fd4eb47e742c8b98241543bf2fa84ee039c00b82
heytitle <pat.chormai@gmail.com>
2020-03-17 22:42:41 +0100

    specify input_shape

--- nets/densenet121.py
+++ nets/densenet121.py
@@ -11,7 +11,7 @@ class Net(BaseNet):
 
         self.encoder = tf.keras.Sequential(
             [
-                DenseNet121(weights=None, include_top=False),
+                DenseNet121(input_shape=in_shape, weights=None, include_top=False),
                 tf.keras.layers.Dense(self.parameters_for_latent),
             ]
         )
