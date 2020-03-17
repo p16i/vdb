d70e8873b19e37b869d6e33138236d009a973e97
heytitle <pat.chormai@gmail.com>
2020-03-17 22:33:57 +0100

    fix densenet121

--- nets/densenet121.py
+++ nets/densenet121.py
@@ -9,7 +9,9 @@ class Net(BaseNet):
     def __init__(self, architecture, in_shape, num_class, cov_type,  batch_norm=True, beta=1e-3, M=1):
         super(Net, self).__init__(architecture, cov_type, num_class, beta, M)
 
-        self.encoder = DenseNet121(
-            weights=None,
-            classes=self.parameters_for_latent
+        self.encoder = tf.keras.Sequential(
+            [
+                DenseNet121(weights=None, include_top=False),
+                tf.keras.layers.Dense(self.parameters_for_latent),
+            ]
         )
